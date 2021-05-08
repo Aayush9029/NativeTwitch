9c60610fce6dc6ea754d9e5dd85538e75fa3f332
Aayush9029 <aayushpokharel36@gmail.com>
2021-05-07 22:12:29 -0400

    Initial Commit

---
+++ NativeTwitch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NativeTwitch
+//
+//  Created by Aayush Pokharel on 2021-05-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NativeTwitch/NativeTwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeTwitchApp.swift
+//  NativeTwitch
+//
+//  Created by Aayush Pokharel on 2021-05-07.
+//
+
+import SwiftUI
+
+@main
+struct NativeTwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
